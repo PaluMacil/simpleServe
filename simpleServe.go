2e5e8e3db113801b55c181e9c4eb8ef110542983
Dan Wolf <dan.wolf@ctgcompanies.com>
2017-02-27 17:01:56 -0500

    Add echo endpoint

--- simpleServe.go
+++ simpleServe.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./static")))
-    http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		requestDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+		}
+		fmt.Fprintf(w, string(requestDump))
+	})
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	fmt.Println("Now serving on port 3033")
+	err := http.ListenAndServe(":3033", nil)
+	if err != nil {
+		panic(err)
+	}
 }
